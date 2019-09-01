9a9d11e9de1b0c3002278c17a0b2d9d84e3125cf
rimon <rimonrpallan@gmail.com>
2019-09-01 11:14:31 +0530

    Initial commit

---
+++ app/src/main/java/com/voyager/boorna1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.voyager.boorna1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/voyager/boorna1/views/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.voyager.boorna1.views;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import com.voyager.boorna1.R;
+import com.voyager.boorna1.databinding.ActivityLoginBinding;
+import com.voyager.boorna1.viewmodels.LoginViewModel;
+
+public class LoginActivity  extends AppCompatActivity {
+
+    ActivityLoginBinding loginBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        loginBinding.setLoginViewModel(new LoginViewModel());
+        loginBinding.executePendingBindings();
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
